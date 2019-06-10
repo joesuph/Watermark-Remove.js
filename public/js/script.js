c816d9f129ce93051b6537f5e760eec53b004755
Joseph <joseph.olsen98@yahoo.com>
2019-06-10 15:03:21 -0700

    added style pic editing

--- public/js/script.js
+++ public/js/script.js
@@ -14,6 +14,7 @@ function drawImage(image) {
 var imageData = context.getImageData(0, 0, canvas.width, canvas.height);
 var data = imageData.data;
 
-for (var i = 0; i < data.length; i++) {
-    data[i] = 255;
+for (var i = 0; i < imageData.data.length; i++) {
+    imageData.data[i] = 255;
 }
+context.putImageData(imageData.data, dx, dy);
