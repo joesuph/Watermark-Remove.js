ef75a6b21bc658f1d267426222e4a4ef4822e157
Joseph <joseph.olsen98@yahoo.com>
2019-06-10 16:42:05 -0700

    added tint functionality

--- public/js/script.js
+++ public/js/script.js
@@ -19,4 +19,38 @@ for (var i = 0; i < imageData.data.length; i += 4) {
     imageData.data[i + 1] = 255 - imageData.data[i+1]
     imageData.data[i + 2] = 255 - imageData.data[i+2]
 }
-context.putImageData(imageData, 0, 0);
+var tint = [Math.floor(Math.random() * 255),Math.floor(Math.random() * 255),Math.floor(Math.random() * 255)]
+var alpha = .25;
+imageData.data = addWatermark(imageData.data,image.width,image.height,tint,alpha);
+
+context.putImageData(imageData,0,0);
+
+function addWatermark(data,dx,dy,tint,alpha)
+{
+  console.log("Tint:");
+  console.log(tint);
+
+  //Get Inidices
+  var indices = {};
+  for (var i=Math.floor(dx /4);i<Math.floor((dx * 3) /4);i++)
+  {
+    var ipoints = new Set();
+    for(var j=0;j<dy;j++)
+    {
+        ipoints.add(j);
+    }
+    indices.set(i,ipoints);
+  }
+  //Apply tint to indices
+  for (var i=0;i<data.length;i += 4)
+  {
+    if (indices.get(Math.floor(i/(4*dx))).has((i/4)%dx))
+    {
+      data[i] = (1-alpha) * data[i] + alpha * tint[0]
+      data[i+1] = (1-alpha) * data[i] + alpha * tint[1]
+      data[i+2] = (1-alpha) * data[i] + alpha * tint[2]
+    }
+  }
+
+  return data;
+}
