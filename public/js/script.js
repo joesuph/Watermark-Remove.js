3da4f196754e4748f5b650c087469329cd6a55ce
Joseph <joseph.olsen98@yahoo.com>
2019-06-10 16:50:08 -0700

    changed test image and undid image inversion

--- public/js/script.js
+++ public/js/script.js
@@ -14,11 +14,6 @@ function drawImage(image) {
 var imageData = context.getImageData(0, 0, canvas.width, canvas.height);
 var data = imageData.data;
 
-for (var i = 0; i < imageData.data.length; i += 4) {
-    imageData.data[i] = 255 - imageData.data[i];
-    imageData.data[i + 1] = 255 - imageData.data[i+1]
-    imageData.data[i + 2] = 255 - imageData.data[i+2]
-}
 var tint = [Math.floor(Math.random() * 255),Math.floor(Math.random() * 255),Math.floor(Math.random() * 255)]
 var alpha = .25;
 imageData.data = addWatermark(imageData.data,image.width,image.height,tint,alpha);
