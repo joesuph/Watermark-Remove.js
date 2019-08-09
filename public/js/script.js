9c44f7706c07dc71ded7b8555dba98a99862d64d
Joseph <joseph.olsen98@yahoo.com>
2019-08-08 19:06:31 -0700

    cleaned up code

--- public/js/script.js
+++ public/js/script.js
@@ -6,11 +6,16 @@ var tint;
 var alpha;
 var paint = false;
 var edget;
+var first;
+var last;
 
 window.addEventListener('load', init);
 
 function init()
 {
+    /********************************************
+    Add Watermark to image and place it in canvas
+    ********************************************/
     image = document.getElementById('img');
     canvas = document.getElementById('canvas');
     context = canvas.getContext('2d');
@@ -21,115 +26,33 @@ function init()
     imageData = context.getImageData(0, 0, canvas.width, canvas.height);
 
     tint = [255,255,255]
-    imageData.data = addWatermark(imageData.data,image.width,image.height,tint);
+    imageData.data = addWatermark(imageData.data,tint);
 
     context.putImageData(imageData,0,0);
-    init2();
-}
+    
 
-
-
-function addWatermark(data,dx,dy,tint)
-{
-  console.log("Tint:");
-  console.log(tint);
-
-  //Get Inidices
-  var maskRatio = Math.random() * .5;
-  var maskimageratio = .3;
-  var maskwidth = maskRatio * image.width * maskimageratio;
-  var maskheight = maskRatio * image.height * maskimageratio;
-  var maskAmount = Math.floor(maskimageratio * image.width * image.height / (maskwidth * maskheight));
-  
-  var indices = new Map();
-  for(var a=0;a<maskAmount;a++)
-  {
-
-    //
-    var rx = Math.floor(image.width * Math.random() * (1-maskwidth/image.width));
-    var ry = Math.floor(image.height * Math.random() * (1 - maskheight/image.height));
-    for (var i=ry;i<ry+maskheight;i++)
-    {
-      if (indices.get(i) == undefined)
-          indices.set(i,new Set());
-      for(var j=rx;j<rx + maskwidth;j++)
-      {
-          indices.get(i).add(j);
-      }
-    }
-  }
-
-  //Apply tint to indices
-  console.log(indices.entries.length)
-  var alpha = Math.random() * .8;
-  for (var i=0;i<data.length;i += 4)
-  {
-    if (indices.get(Math.floor(i/(4*image.width))) != undefined)
-        if(indices.get(Math.floor(i/(4*image.width))).has((i/4)%image.width))
-        {
-          data[i] = (1-alpha) * data[i] + alpha * tint[0]
-          data[i+1] = (1-alpha) * data[i+1] + alpha * tint[1]
-          data[i+2] =(1-alpha) * data[i+2] + alpha * tint[2]
-        }
-  }
-
-  return data;
-}
-
-
-function getCursorPosition(canvas, event) {
-  const rect = canvas.getBoundingClientRect()
-  const x = event.clientX - rect.left
-  const y = event.clientY - rect.top
-  return [x,y];
-}
-
-function findPos(e,obj) {
-  var curleft = 0, curtop = 0;
-  if (obj.offsetParent) {
-      do {
-          curleft += obj.offsetLeft;
-          curtop += obj.offsetTop;
-      } while (obj = obj.offsetParent);
-      return [e.pageX-curleft, e.pageY-curtop];
-  }
-  return undefined;
-}
-
-/*
-const canvas = document.querySelector('canvas')
-canvas.addEventListener('mousedown', function(e) {
-    point = getCursorPosition(canvas, e);
-    imageData.data
-})
-*/
-
-var last;
-var first;
-function init2(){
-    //canvas.onmousedown = (e)=>{paint=true;edget = new Set([getCursorPosition(canvas,e)]);last = getCursorPosition(canvas,e);}
-    canvas.onmousedown = (e)=>{paint=true;edget = new Set([findPos(e,canvas)]);first = findPos(e,canvas);last =findPos(e,canvas);}
+    /*********************************************
+     Set up event handlers for watermark selecting
+     *********************************************/
+    canvas.onmousedown = (e)=>{paint=true;edget = new Set([intify(findPos(e,canvas))]);first = findPos(e,canvas);last =findPos(e,canvas);}
     canvas.onmousemove = (e)=>{
       if(paint)
       {
         var data = imageData.data;
         console.log('move');
-        //var p1 = getCursorPosition(canvas,e);
         var p1 = findPos(e,canvas);
-
-        p1 = [Math.round(p1[0]),Math.round(p1[1])];
         var lp = getLinePoints(last,p1);
         last = p1;
         for(var i=0;i<lp.length;i++)
         {
-          edget.add(lp[i]);
+          edget.add(intify(lp[i]));
           data[(lp[i][1]*image.width + lp[i][0])*4] = 0;
           data[(lp[i][1]*image.width + lp[i][0])*4+1] = 255;
           data[(lp[i][1]*image.width +lp[i][0])*4+2] = 0;
           imageData.data = data;
           
         }
-        edget.add(p1);
+        edget.add(intify(p1));
 
         data[(p1[1]*image.width + p1[0])*4] = 0;
         data[(p1[1]*image.width + p1[0])*4+1] = 255;
@@ -146,7 +69,7 @@ function init2(){
       lp = getLinePoints(first,last);
       for(var i=0;i<lp.length;i++)
       {
-        edget.add(lp[i]);
+        edget.add(intify(lp[i]));
         data[(lp[i][1]*image.width + lp[i][0])*4] = 0;
         data[(lp[i][1]*image.width + lp[i][0])*4+1] = 255;
         data[(lp[i][1]*image.width +lp[i][0])*4+2] = 0;
@@ -157,6 +80,104 @@ function init2(){
     }
 }
 
+
+/*************************************************
+ * Adds watermark to imageData
+ *  params = (imageData,tint_of_desired_watermark)
+ *************************************************/
+function addWatermark(data,tint)
+{
+  //Get Inidices
+  var maskRatio = Math.random() * .5;
+  var maskimageratio = .3;
+  var maskwidth = maskRatio * image.width * maskimageratio;
+  var maskheight = maskRatio * image.height * maskimageratio;
+  var maskAmount = Math.floor(maskimageratio * image.width * image.height / (maskwidth * maskheight));
+  
+  var indices = new Map();
+  for(var a=0;a<maskAmount;a++)
+  {
+    var rx = Math.floor(image.width * Math.random() * (1-maskwidth/image.width));
+    var ry = Math.floor(image.height * Math.random() * (1 - maskheight/image.height));
+    for (var i=ry;i<ry+maskheight;i++)
+    {
+      if (indices.get(i) == undefined)
+          indices.set(i,new Set());
+      for(var j=rx;j<rx + maskwidth;j++){indices.get(i).add(j);}
+    }
+  }
+
+  //Apply tint to indices
+  var alpha = Math.random() * .8;
+  for (var i=0;i<data.length;i += 4)
+  {
+    if (indices.get(Math.floor(i/(4*image.width))) != undefined)
+        if(indices.get(Math.floor(i/(4*image.width))).has((i/4)%image.width))
+        {
+          data[i] = (1-alpha) * data[i] + alpha * tint[0]
+          data[i+1] = (1-alpha) * data[i+1] + alpha * tint[1]
+          data[i+2] =(1-alpha) * data[i+2] + alpha * tint[2]
+        }
+  }
+
+  return data;
+}
+
+
+/**********************************
+ * Get Position of mouse in canvas
+ *    params = (event, canvas)
+ ***********************************/
+function findPos(e,obj) {
+  var curleft = 0, curtop = 0;
+  if (obj.offsetParent) {
+      do {
+          curleft += obj.offsetLeft;
+          curtop += obj.offsetTop;
+      } while (obj = obj.offsetParent);
+      return [e.pageX-curleft, e.pageY-curtop];
+  }
+  return undefined;
+}
+
+/**********************************************
+ * Returns inner pixel from a set of pixel edges
+ *  params = (edge_set_as_ints)
+ ***********************************************/
+function getInner(edges)
+{
+  var edd;
+  for(var i=0;i<edges.length;i++){edd.push(pointify(edges[i]))}
+  var arr = edd.sort((a,b)=>{return a[0]-b[0]});
+  var left = arr[0][0];
+  var avg = (arr[1][1] + arr[2][1])/2;
+  return [left+1,Math.round(avg)];
+}
+
+/*******************************************************
+ * Convert to and from point and integer representations
+ ******************************************************/
+function intify(point){return point[0] + point[1] * image.width;}
+function pointify(num){return [num % image.width,Math.floor(num / image.width)];}
+
+/*******************************************************
+ * Get integer representation of pixels within edge list
+ * starting from innerpoint
+ *******************************************************/
+function fill(edges,inner)
+{
+  var visited = new Set();
+  var visit_later=[inner];
+  while(visit_later.length != 0)
+  {
+    visit__later
+  }
+}
+
+
+/***********************************************************
+ * Get the pixels along the straight line between two points
+ ************************************************************/
 function getLinePoints(p1,p2)
 {
 	var slope;
@@ -186,19 +207,10 @@ function getLinePoints(p1,p2)
   // yn = slope*(xn-xo)+yo
   while(s<e)
 	{
-	if(horiz)nps.push([s,Math.round((s-b)*slope+original)])
-  else nps.push([Math.round((s-b)*slope+original),s])
-  
-	s+= 1;
+	  if(horiz)nps.push([s,Math.round((s-b)*slope+original)])
+    else nps.push([Math.round((s-b)*slope+original),s])
+	  s+= 1;
 	}
 	return nps;
   
 }
-
-
-//onmousedown start paint
-//while paint, on mousemove add pixel to edgeList
-//on mouseup connect last pixel to first pixel with edgePixels, toggle paint
-
-//on remove watemark button, get list of pixels in the region
-//Add a rgb color dial and and alpha dial and edit the pixels in the region
